2c0b66acddab0cb3bd41731b5331e1943c061079
jaimecl208 <jaimecl1403@gmail.com>
2024-10-05 18:24:41 +0200

    a

--- src/pages/backend/test2.js
+++ src/pages/backend/test2.js
@@ -2,10 +2,8 @@
 
 export async function GET(context) {
   const db = context.locals.runtime.env.DB;
-  const result = await db.prepare("SELECT * FROM usuarios WHERE id = 0;").run();
+  const result = await db.prepare("SELECT * FROM usuarios").run();
   console.log(result.results);
-  const result2 = await db.prepare("SELECT * FROM usuarios WHERE id = 1;").run();
-  console.log(result2.results);
 
   return new Response(JSON.stringify(result.results), { status: 500 });
 }
