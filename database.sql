f9ab175dfaec9124c4b74bfd333f7c64e339e39a
jaimecl208 <jaimecl1403@gmail.com>
2024-10-05 18:31:14 +0200

    a

--- database.sql
+++ database.sql
@@ -1,5 +1,3 @@
 PRAGMA defer_foreign_keys=TRUE;
-CREATE TABLE [usuarios] ("id" integer PRIMARY KEY,"nombre" text,"puntos" integer);
-CREATE TABLE [dispositivos] ("id" integer PRIMARY KEY,"nimagen" integer);
-INSERT INTO usuarios VALUES(,"PabloGameplays", 999999);
-INSERT INTO usuarios VALUES(,"PabloGameplays2", 999999);
+CREATE TABLE [usuarios] ("id" integer PRIMARY KEY autoincrement,"nombre" text,"puntos" integer);
+CREATE TABLE [dispositivos] ("id" integer PRIMARY KEY auntoincrement,"nimagen" integer);
