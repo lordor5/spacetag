4b607e573823eecb302a7d8a8a5e4ea84b4f7491
Lordor5 <thelordor5@gmail.com>
2024-10-05 18:59:22 +0200

    awk.jdlwa

--- database.sql
+++ database.sql
@@ -1,4 +1,8 @@
 PRAGMA defer_foreign_keys=TRUE;
 CREATE TABLE [usuarios] ("id" integer PRIMARY KEY,"nombre" text,"puntos" integer);
-CREATE TABLE [dispositivos] ("id" integer PRIMARY KEY,"nimagen" integer);
+CREATE TABLE [dispositivos] ("id" integer PRIMARY KEY,"imagen" integer);
 INSERT INTO usuarios VALUES(0,"PabloGameplays", 999999);
+INSERT INTO dispositivos VALUES(0, 1);
+INSERT INTO dispositivos VALUES(1, 2);
+INSERT INTO dispositivos VALUES(2, 3);
+INSERT INTO dispositivos VALUES(3, 4);
