6271715026260f4f5bb83905e43780b1b5dca1b0
jaimecl208 <jaimecl1403@gmail.com>
2024-10-05 18:32:25 +0200

    a

--- src/pages/backend/test2.js
+++ src/pages/backend/test2.js
@@ -4,7 +4,7 @@ export async function GET(context) {
   const db = context.locals.runtime.env.DB;
   //const result = await db.prepare("SELECT * FROM usuarios").run();
   //console.log(result.results);
-  const write = await db.prepare("INSERT INTO usuarios (id, nombre, puntos) value (?1, ?2)").bind(NULL, "pablogamer", 100).run();
+  const write = await db.prepare("INSERT INTO usuarios (nombre, puntos) value (?1, ?2)").bind("pablogamer", 100).run();
 
   return new Response(JSON.stringify(result.results), { status: 500 });
 }
