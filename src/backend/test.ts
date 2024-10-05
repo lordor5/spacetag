1de82402f5e23eb92af346b7cae4c61412e9931a
jaimecl208 <jaimecl1403@gmail.com>
2024-10-05 15:58:29 +0200

    hola

--- src/backend/test.ts
+++ src/backend/test.ts
@@ -1,12 +1,12 @@
 import type { APIRoute } from "astro";
 
+var idsfotos = { 1:"a", 2:"a", 3:"a", 4:"a", 5:"a" }
+
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
-  const name = data.get("name");
-  const email = data.get("email");
-  const message = data.get("message");
+  const nmovil = data.get("nmovil");
   // Validate the data - you'll probably want to do more than this
-  if (!name || !email || !message) {
+  if (!nmovil) {
     return new Response(
       JSON.stringify({
         message: "Missing required fields",
@@ -15,6 +15,14 @@ export const POST: APIRoute = async ({ request }) => {
     );
   }
   // Do something with the data, then return a success response
+  //const query1 = db.prepare("SELECT MAX(ID) from ID");
+  //const maxid = await query1.first("maxid");
+
+
+
+
+  const query = db.prepare("INSERT INTO ID (ID, NumeroFoto) VALUES (?1, ?2) ON CONFLICT (ID) DO UPDATE SET NumeroFoto = ?2;").bind(nmovil,0).run();
+
   return new Response(
     JSON.stringify({
       message: "Success!"
