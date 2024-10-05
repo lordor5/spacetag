12e97b93962059b19461d99a002867f8c40b6b47
Lordor5 <thelordor5@gmail.com>
2024-10-05 16:46:59 +0200

    aflhlñhdflks

--- src/pages/backend/test2.js
+++ src/pages/backend/test2.js
@@ -2,7 +2,8 @@
 
 export async function GET(context) {
   const db = context.locals.runtime.env.DB;
-  const result = await db.prepare("SELECT * FROM cosas WHERE cosa = 12;").run();
+  const result = await db.prepare("SELECT * FROM cosas WHERE cosa = 12;").run()
+    .results;
   console.log(db);
   console.log(result);
   //   try {
