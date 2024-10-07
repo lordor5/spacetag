6248fd15565c32ccf0593dfc0494a5e0ef91e420
Lordor5 <thelordor5@gmail.com>
2024-10-07 14:32:36 +0200

    ahora si final

--- src/pages/backend/test2.js
+++
@@ -1,9 +0,0 @@
-//import type { APIContext } from "astro";
-
-// export async function GET(context) {
-//   const db = context.locals.runtime.env.DB;
-//   const result = await db.prepare("SELECT * FROM dispositivos").run();
-//   console.log(result.results);
-
-//   return new Response(JSON.stringify(result.results), { status: 500 });
-// }
